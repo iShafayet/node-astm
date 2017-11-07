c8b41dc1e891fdb2e2adbe2bbc9a376a0afe94d5
Sayem Shafayet <ishafayet@gmail.com>
2017-11-07 14:51:01 +0600

    Created HoribaPenta60 Class

---
+++ horiba-penta-60.js
@@ -0,0 +1,147 @@
+
+var EventEmitter = require('events');
+var SerialPort = require('serialport');
+
+const ACK_BUFFER = new Buffer([6]);
+const ENQ = 5;
+const STX = 2;
+const ETX = 3;
+const LF = 10;
+const CR = 13;
+const EOT = 4;
+
+class HoribaPenta60 extends EventEmitter {
+
+  constructor() {
+    super();
+    this.port = null;
+  }
+
+  initiate(portString) {
+    Object.assign(this, { portString });
+
+    this.transmission = [];
+    this.statement = null;
+
+    this.port = new SerialPort(portString, {
+      baudRate: 38400,
+      autoOpen: false
+    });
+
+    this.port.on('open', _ => this.__handleOpen());
+
+    this.port.on('error', err => this.__handleError(err));
+
+    this.port.open(err => {
+      if (err) {
+        this.__handleError(err, 'Error opening port:');
+      }
+    });
+
+    this.port.on('data', err => this.__handleData(data));
+  }
+
+  __log(...data) {
+    data.unshift('HoribaPenta60>');
+    this.emit('log', ...data);
+  }
+
+  __error(err) {
+    this.emit('error', err);
+  }
+
+  __parseError(err){
+    this.emit('parse-error', err);
+  }
+
+  __handleOpen() {
+    this.__log('opened on', this.portString);
+    this.emit('open');
+  }
+
+  __handleError(err, prefix = 'error:') {
+    this.__log(prefix, err.message);
+    this.__error(err);
+  }
+
+  __handleData(data) {
+    let str = data.toString('ascii');
+
+    if (str.length === 0) return;
+
+    if (str.charCodeAt(0) === ENQ) {
+      this.port.write(ACK_BUFFER);
+
+    } else if (str.charCodeAt(0) === EOT) {
+      // console.log('this.transmission', this.transmission);
+      this.emit('data', transmission);
+      this.__log('transmission:', this.summarizeTransmission(transmission));
+      this.transmission = [];
+
+    } else {
+      for (char of str.split('')) {
+        // console.log(char, char.charCodeAt(0));
+
+        if (char.charCodeAt(0) === STX) {
+          this.statement = {
+            hasStarted: false,
+            hasEnded: false,
+            dataMessage: '',
+            checksum: ''
+          }
+          this.statement.hasStarted = true;
+
+        } else if (char.charCodeAt(0) === ETX) {
+          if (!this.statement.hasStarted) {
+            this.__parseError("this.statement ended before it was started.");
+            return;
+          }
+          this.statement.hasEnded = true;
+
+        } else if (char.charCodeAt(0) === LF) {
+          if (!this.statement.hasStarted) {
+            this.__parseError("LF before this.statement was started.");
+            return;
+          }
+          if (!this.statement.hasEnded) {
+            this.__parseError("LF before this.statement was ended.");
+            return;
+          }
+          this.transmission.push(this.statement);
+          this.port.write(ACK_BUFFER);
+
+        } else {
+          if (!this.statement.hasStarted) {
+            this.__parseError(`Unkown character received before this.statement was started, ${char}, ${char.charCodeAt()}`);
+            return;
+          }
+          if (char.charCodeAt(0) !== CR) {
+            if (!this.statement.hasEnded) {
+              this.statement.dataMessage += char;
+            } else {
+              this.statement.checksum += char;
+            }
+          }
+        }
+      }
+
+    }
+  }
+
+  summarizeTransmission(transmission) {
+    text = '';
+    for (statement of transmission) {
+      let dataMessage = statement.dataMessage;
+      if (dataMessage.length > 0) {
+        dataMessage = dataMessage.substr(1, dataMessage.length);
+      }
+      text += dataMessage + '\n';
+    }
+    return text;
+  }
+
+
+
+}
+
+exports.HoribaPenta60 = HoribaPenta60;
--- index.js
+++ index.js
@@ -1,103 +1,25 @@
-var SerialPort = require('serialport');
-var port = new SerialPort('COM4', {
-  baudRate: 38400,
-  autoOpen: false
+
+
+
+let { HoribaPenta60 } = require('./horiba-penta-60');
+
+let machine = new HoribaPenta60();
+
+machine.on('log', (...args)=> {
+  console.log(...args);
 });
 
-const ACK_BUFFER = new Buffer([6]);
-const ENQ = 5;
-// const ETB = new Buffer([23]);
-// const NAK = new Buffer([21]);
-const STX = 2;
-const ETX = 3;
-const LF = 10;
-const CR = 13;
-const EOT = 4;
-
-port.open(err => {
-  if (err) {
-    console.log('Error opening port: ', err.message);
-  }
+machine.on('error', (error)=> {
+  console.log(error);
 });
 
-port.on('error', err => {
-  console.log('Error:', err);
+machine.on('parse-error', (error)=> {
+  console.log(error);
 });
 
-port.on('open', _ => {
-  console.log("PORT", "opened")
-});
+machine.on('data',(transmission)=>{
+  // console.log('transmission:', transmission);
+})
 
-let summarizeTransmission = (transmission)=>{
-  text = '';
-  for (statement of transmission){
-    text += statement.dataMessage + '\n';
-  }
-  return text;
-};
-
-let transmission = []
-let statement = null
-port.on('data', function (data) {
-  // console.log('Raw Data:', data);
-
-  let str = data.toString('ascii');
-
-  if (str.length === 0) return;
-
-  if (str.charCodeAt(0) === ENQ) {
-    port.write(ACK_BUFFER);
-
-  } else if (str.charCodeAt(0) === EOT) {
-    // console.log('transmission', transmission);
-    console.log(summarizeTransmission(transmission));
-    transmission = [];
-
-  } else {
-    for (char of str.split('')) {
-      // console.log(char, char.charCodeAt(0));
-
-      if (char.charCodeAt(0) === STX) {
-        statement = {
-          hasStarted: false,
-          hasEnded: false,
-          // lastChar: '',
-          dataMessage: '',
-          checksum: ''
-        }
-        statement.hasStarted = true;
-
-      } else if (char.charCodeAt(0) === ETX) {
-        if (!statement.hasStarted) {
-          throw new Error("Statement ended before it was started.");
-        }
-        statement.hasEnded = true;
-
-      } else if (char.charCodeAt(0) === LF) {
-        if (!statement.hasStarted) {
-          throw new Error("LF before statement was started.");
-        }
-        if (!statement.hasEnded) {
-          throw new Error("LF before statement was ended.");
-        }
-        transmission.push(statement);
-        port.write(ACK_BUFFER);
-
-      } else {
-        if (!statement.hasStarted) {
-          throw new Error(`Unkown character received before statement was started, ${char}, ${char.charCodeAt()}`);
-        }
-        if (char.charCodeAt(0) !== CR) {
-          if (!statement.hasEnded) {
-            statement.dataMessage += char;
-          } else {
-            statement.checksum += char;
-          }
-        }
-      }
-    }
-
-  }
-
-});
+machine.initiate('COM4')
 
